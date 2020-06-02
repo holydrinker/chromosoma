57648b0803572e1b3b8e249ce07d8c9e5ccabc4d
Peppo Lorusso <giuseppelorusso92@gmail.com>
2020-06-02 15:56:52 +0200

    [#14] csv writer first implementation

---
+++ src/test/scala/holydrinker/chromosoma/writers/AvroWriterSuite.scala
@@ -0,0 +1,45 @@
+package holydrinker.chromosoma.writers
+
+import java.io.File
+import java.nio.file.Paths
+
+import com.sksamuel.avro4s.{ AvroInputStream, AvroSchema, RecordFormat }
+import holydrinker.chromosoma.TempDirSupport
+import holydrinker.chromosoma.model.Dataset
+import munit.FunSuite
+
+class AvroWriterSuite extends FunSuite with TempDirSupport {
+
+  test("avro writer") {
+    withTempDir { tempDir =>
+      // Generate dataset
+      case class Artist(name: String, surname: String, age: Int, instrument: String)
+      val artistSchema = AvroSchema[Artist]
+      val artistFormat = RecordFormat[Artist]
+      val deserializedArtists = Seq(
+        Artist("simon", "neil", 41, "guitar"),
+        Artist("martin", "mendez", 42, "bass")
+      )
+      val serializedArtists = deserializedArtists.map(artist => artistFormat.to(artist))
+      val artistDataset     = Dataset(schema = artistSchema, rows = serializedArtists)
+
+      // Write dataset
+      val resultPath = Paths.get(tempDir.getAbsolutePath, "result.avro").toAbsolutePath.toString
+      new AvroWriter().save(artistDataset, resultPath)
+
+      // Check dataset
+      val is = AvroInputStream
+        .data[Artist]
+        .from(new File(resultPath))
+        .build(artistSchema)
+
+      val actualDeserializedArtists = is.iterator.toSet
+
+      is.close()
+
+      val expectedDeserializedArtists = deserializedArtists.toSet
+
+      assert(actualDeserializedArtists == expectedDeserializedArtists)
+    }
+  }
+}
